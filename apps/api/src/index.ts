067fc35873759de34a3c6ad554cce27bd85b642e
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-12-28 15:23:57 -0300

    feat: configure database connection

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,2 +1,24 @@
-import fastify from 'fastify';
+import Fastify from 'fastify';
+import mongoose from 'mongoose';
+
+const DATABASE_URL = 'mongodb://mongo:docker@localhost:27017';
+
+const app = Fastify({ logger: true })
+
+mongoose.connect(DATABASE_URL).then(() => {
+  app.log.debug('test')
+
+  app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Hello World' })
+  });
+
+  app.listen({ port: 3000 }, (err) => {
+    if (err) {
+      app.log.error(err)
+      process.exit(1)
+    }
+  });
+})
+.catch(app.log.error)
+.finally(mongoose.disconnect);
 
