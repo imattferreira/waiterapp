366c95658db9b0ec1f40f0b6656c40db053b726c
MattFerreira18 <matheusferreira.dev@gmail.com>
2023-01-08 16:47:34 -0300

    chore(api): remove unused modules

--- apps/api/src/v2/infra/http/configure.ts
+++
@@ -1,9 +0,0 @@
-function configure(
-  configs: Record<string, unknown> | Record<string, unknown>[]
-) {
-  return Array.isArray(configs)
-    ? configs.reduce((acc, curr) => ({ ...acc, ...curr }), {})
-    : configs;
-}
-
-export default configure;
--- apps/api/src/v2/infra/http/middlewares/with-middlewares.ts
+++
@@ -1,9 +0,0 @@
-import { MiddlewareFn } from "../interfaces";
-
-function withMiddlewares(middlewares: MiddlewareFn | MiddlewareFn[]) {
-  return {
-    preHandler: Array.isArray(middlewares) ? middlewares : [middlewares],
-  };
-}
-
-export default withMiddlewares;
