f48186026371c23524249e7976c842b2213651cd
MattFerreira18 <matheusferreira.dev@gmail.com>
2023-02-18 23:11:57 -0300

    fix(vite.config.ts): add missing config to suport import alias

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import path from 'node:path';
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 });
